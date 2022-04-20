452b0c9dccb82acb1a2e57424df509d17999312d
eiji <eijishimizu9@gmail.com>
2022-04-20 19:56:26 +0900

    accept後の処理を別スレッドに渡す

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,17 +6,99 @@
 #include <cstring>
 #include <exception>
 #include <iostream>
+#include <map>
+#include <mutex>
 #include <sstream>
 #include <string>
+#include <thread>
 #include <vector>
 
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
+class ThreadsMap {
+public:
+    ThreadsMap(int max)
+        : max_{max}
+    {
+    }
+    ThreadsMap()
+        : max_{10}
+    {
+    }
+    ~ThreadsMap()
+    {
+        // noop
+    }
+
+    // コピー禁止
+    ThreadsMap(const ThreadsMap &) = delete;
+    ThreadsMap &operator=(const ThreadsMap &) = delete;
+    // ムーブ禁止
+    ThreadsMap(ThreadsMap &&) = delete;
+    ThreadsMap &operator=(ThreadsMap &&) = delete;
+
+    size_t size() const
+    {
+        // この関数はデバッグ用なのでロック無し
+        return threads_.size();
+    }
+
+    bool isFull()
+    {
+        std::lock_guard<std::mutex> lock{mt_};
+        return threads_.size() == max_;
+    }
+
+    bool addThread(std::thread &&t)
+    {
+        std::lock_guard<std::mutex> lock{mt_};
+        if (threads_.size() < max_) {
+            threads_.insert(std::make_pair(t.get_id(), std::move(t)));
+            finishedFlag_.try_emplace(t.get_id(), false);
+            return true;
+        }
+        return false;
+    }
+
+    void setFinishedFlag(const std::thread::id id)
+    {
+        std::lock_guard<std::mutex> lock{mt_};
+        finishedFlag_.insert_or_assign(id, true);
+    }
+
+    void cleanUp()
+    {
+        std::lock_guard<std::mutex> lock{mt_};
+        // スレッド数が最大の半分より多ければクリーンアップする
+        if (threads_.size() > (max_ / 2)) {
+            std::vector<std::thread::id> vec;
+            for (const auto &p : finishedFlag_) {
+                if (p.second) {
+                    vec.push_back(p.first);
+                    // finishedFlgがtrueのスレッドのみjoin
+                    std::cout << "thread id : " << p.first << " join for cleanup." << std::endl;
+                    threads_.at(p.first).join();
+                }
+            }
+            for (const auto id : vec) {
+                threads_.erase(id);
+                finishedFlag_.erase(id);
+            }
+        }
+    }
+
+private:
+    const int max_;
+    std::map<std::thread::id, std::thread> threads_;
+    std::map<std::thread::id, bool> finishedFlag_;
+    std::mutex mt_;
+};
+
 int main()
 {
-    const int DEFAULT_BUFLEN = 512;
     const std::string DEFAULT_PORT = "27015";
+    const int MAX_THREADS = 10;
 
     try {
         WSADATA wsaData;
@@ -27,9 +109,9 @@ int main()
         struct addrinfo *result = NULL;
         struct addrinfo hints;
 
-        int iSendResult;
-        char buf[DEFAULT_BUFLEN];
-        char recvBuf[DEFAULT_BUFLEN];
+        // int iSendResult;
+        // char buf[DEFAULT_BUFLEN];
+        // char recvBuf[DEFAULT_BUFLEN];
 
         // winsockの初期化
         iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -82,20 +164,22 @@ int main()
         }
 
         // 仮の応答用メッセージ
-        memset(buf, 0, sizeof(buf));
-        std::ostringstream oss{""};
-        oss << "HTTP/1.0 200 OK\r\n"
-            << "Content-Length: 20\r\n"
-            << "Content-Type: text/html\r\n"
-            << "\r\n"
-            << "HELLO\r\n";
+        // memset(buf, 0, sizeof(buf));
+        // std::ostringstream oss{""};
+        // oss << "HTTP/1.0 200 OK\r\n"
+        //     << "Content-Length: 20\r\n"
+        //     << "Content-Type: text/html\r\n"
+        //     << "\r\n"
+        //     << "HELLO\r\n";
 
-        std::cout << "here" << std::endl;
+        ThreadsMap threads;
         while (1) {
 
-            std::cout << "there" << std::endl;
-
+            std::cout << "accept BEFORE" << std::endl;
             clientSocket = accept(listenSocket, NULL, NULL);
+            std::cout << "accept AFTER" << std::endl;
+            std::cout << "clientSocket is : " << clientSocket << std::endl;
+
             if (clientSocket == INVALID_SOCKET) {
                 std::cout << "accept failed with error: " << WSAGetLastError() << std::endl;
                 closesocket(listenSocket);
@@ -103,39 +187,80 @@ int main()
                 return 1;
             }
 
-            do {
-                memset(recvBuf, 0, sizeof(recvBuf));
-                iResult = recv(clientSocket, recvBuf, sizeof(recvBuf), 0);
-                if (iResult > 0) {
-                    // 本来ならばクライアントからの要求内容をパースすべきです
-                    std::cout << recvBuf << std::endl;
+            if (threads.isFull()) {
+                std::cout << "number of threads is upper limits." << std::endl;
+                closesocket(listenSocket);
+                return 1;
+            }
 
-                    // 相手が何を言おうとダミーHTTPメッセージ送信
-                    size_t len = sizeof(buf);
-                    if (sizeof(buf) > oss.str().size()) {
-                        len = oss.str().size();
-                    }
-                    std::memcpy(buf, oss.str().c_str(), len);
-                    iSendResult = send(clientSocket, buf, iResult, 0);
-                    if (iSendResult == SOCKET_ERROR) {
-                        std::cout << "send failed with error: " << WSAGetLastError() << std::endl;
-                        closesocket(clientSocket);
-                        WSACleanup();
-                        return 1;
-                    }
-                }
-                else if (iResult == 0) {
-                    std::cout << "Connection closing..." << std::endl;
-                }
-                else {
-                    std::cout << "recv failed with error: " << WSAGetLastError() << std::endl;
+            std::thread t{[clientSocket, &threads] {
+                const int DEFAULT_BUFLEN = 512;
+
+                try {
+                    char buf[DEFAULT_BUFLEN];
+                    char recvBuf[DEFAULT_BUFLEN];
+                    int result;
+                    int iSendResult;
+
+                    // 仮の応答用メッセージ
+                    memset(buf, 0, sizeof(buf));
+                    std::ostringstream oss{""};
+                    oss << "HTTP/1.0 200 OK\r\n"
+                        << "Content-Length: 20\r\n"
+                        << "Content-Type: text/html\r\n"
+                        << "\r\n"
+                        << "HELLO\r\n";
+
+                    do {
+                        memset(recvBuf, 0, sizeof(recvBuf));
+                        result = recv(clientSocket, recvBuf, sizeof(recvBuf), 0);
+                        if (result > 0) {
+                            // 本来ならばクライアントからの要求内容をパースすべきです
+                            // std::cout << recvBuf << std::endl;
+
+                            // 相手が何を言おうとダミーHTTPメッセージ送信
+                            size_t len = sizeof(buf);
+                            if (sizeof(buf) > oss.str().size()) {
+                                len = oss.str().size();
+                            }
+                            std::memcpy(buf, oss.str().c_str(), len);
+                            iSendResult = send(clientSocket, buf, result, 0);
+                            if (iSendResult == SOCKET_ERROR) {
+                                std::cout << "send failed with error: " << WSAGetLastError() << std::endl;
+                                closesocket(clientSocket);
+                                // WSACleanup();
+                                // return 1;
+                            }
+                        }
+                        else if (result == 0) {
+                            std::cout << "Connection closing..." << std::endl;
+                        }
+                        else {
+                            std::cout << "recv failed with error: " << WSAGetLastError() << std::endl;
+                            closesocket(clientSocket);
+                            // WSACleanup();
+                            // return 1;
+                        }
+                    } while (result > 0);
+
                     closesocket(clientSocket);
-                    WSACleanup();
-                    return 1;
                 }
-            } while (iResult > 0);
+                catch (std::exception &e) {
+                    // ignore
+                    closesocket(clientSocket);
+                }
+                catch (...) {
+                    // ignore
+                    closesocket(clientSocket);
+                }
 
-            closesocket(clientSocket);
+                closesocket(clientSocket);
+                threads.setFinishedFlag(std::this_thread::get_id());
+            }};
+
+            threads.addThread(std::move(t));
+            threads.cleanUp();
+            std::cout << "number of threads is : " << threads.size() << std::endl;
         }
 
         closesocket(listenSocket);
