d6e8883b317a011769fc3e1573852b067f9be0c3
eiji-shimizu <eijishimizu9@gmail.com>
2022-05-17 13:23:19 +0900

    updateとdelete機能のテスト実施

--- test/DatabaseTest.cpp
+++ test/DatabaseTest.cpp
@@ -13,7 +13,10 @@
 #include "Logger.h"
 
 #include <algorithm>
+#include <filesystem>
+#include <fstream>
 #include <string>
+#include <vector>
 
 namespace PapierMache::DbStuff {
 
@@ -33,6 +36,13 @@ namespace PapierMache::DbStuff {
 
         void TearDown() override
         {
+            // 空のデータファイルを新規作成する
+            std::vector<std::string> files = {"user", "order"};
+            std::string dataFilePath = "./database/data/";
+            for (const std::string fileName : files) {
+                std::filesystem::remove(dataFilePath + fileName);
+                std::ofstream ofs{dataFilePath + fileName};
+            }
         }
 
         std::string dq(const std::string &s) const
@@ -59,19 +69,26 @@ namespace PapierMache::DbStuff {
         Driver driver{con};
         Driver::Result r = driver.sendQuery("please:user admin adminpass");
         LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
         r = driver.sendQuery("please:transaction   ");
         LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
         r = driver.sendQuery("please:insert  order (ORDER_NAME=" + dq("order1") + ", CUSTOMER_NAME=" + dq("お客様A") + ", PRODUCT_NAME=" + dq("商品いろはにほへと") + ")");
         LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
         r = driver.sendQuery("please:insert  order (ORDER_NAME=" + dq("order2") + ", CUSTOMER_NAME=" + dq("お客様B") + ", PRODUCT_NAME=" + dq("商品えひもせすん") + ")");
         LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
         r = driver.sendQuery("please:commit");
         LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
 
         r = driver.sendQuery("please:transaction");
         LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
         r = driver.sendQuery("please: select order   ");
         LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
         ASSERT_EQ(2, r.rows.size());
         std::sort(r.rows.begin(), r.rows.end(),
                   [](std::map<std::string, std::string> a, std::map<std::string, std::string> b) {
@@ -95,6 +112,249 @@ namespace PapierMache::DbStuff {
                 ASSERT_FALSE(true);
             }
         }
+    }
+
+    TEST_F(DatabaseTest, update_001)
+    {
+        Database db{};
+        db.start();
+        PapierMache::DbStuff::Connection con = db.getConnection();
+        Driver driver{con};
+        Driver::Result r = driver.sendQuery("please:user admin adminpass");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:transaction   ");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:insert  order (ORDER_NAME=" + dq("order1") + ", CUSTOMER_NAME=" + dq("お客様A") + ", PRODUCT_NAME=" + dq("商品いろはにほへと") + ")");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:insert  order (ORDER_NAME=" + dq("order2") + ", CUSTOMER_NAME=" + dq("お客様B") + ", PRODUCT_NAME=" + dq("商品えひもせすん") + ")");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:insert  order (ORDER_NAME=" + dq("order3") + ", CUSTOMER_NAME=" + dq("お客様B") + ", PRODUCT_NAME=" + dq("商品XYZ") + ")");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:commit");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+
+        r = driver.sendQuery("please:transaction");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:update order  (PRODUCT_NAME=" + dq("テスト商品００８") + ") (CUSTOMER_NAME=" + dq("お客様B") + ") ");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:commit");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+
+        r = driver.sendQuery("please:transaction");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please: select order   ");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        ASSERT_EQ(3, r.rows.size());
+        std::sort(r.rows.begin(), r.rows.end(),
+                  [](std::map<std::string, std::string> a, std::map<std::string, std::string> b) {
+                      return a.at("order_name") < b.at("order_name");
+                  });
+
+        for (int i = 0; i < r.rows.size(); ++i) {
+            const auto row = r.rows[i];
+            switch (i) {
+            case 0:
+                ASSERT_STREQ("order1", row.at("order_name").c_str());
+                ASSERT_STREQ("お客様A", row.at("customer_name").c_str());
+                ASSERT_STREQ("商品いろはにほへと", row.at("product_name").c_str());
+                break;
+            case 1:
+                ASSERT_STREQ("order2", row.at("order_name").c_str());
+                ASSERT_STREQ("お客様B", row.at("customer_name").c_str());
+                ASSERT_STREQ("テスト商品００８", row.at("product_name").c_str());
+                break;
+            case 2:
+                ASSERT_STREQ("order3", row.at("order_name").c_str());
+                ASSERT_STREQ("お客様B", row.at("customer_name").c_str());
+                ASSERT_STREQ("テスト商品００８", row.at("product_name").c_str());
+                break;
+            default:
+                ASSERT_FALSE(true);
+            }
+        }
+    }
+
+    TEST_F(DatabaseTest, update_002)
+    {
+        Database db{};
+        db.start();
+        PapierMache::DbStuff::Connection con = db.getConnection();
+        Driver driver{con};
+        Driver::Result r = driver.sendQuery("please:user admin adminpass");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:transaction   ");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:insert  order (ORDER_NAME=" + dq("order1") + ", CUSTOMER_NAME=" + dq("お客様A") + ", PRODUCT_NAME=" + dq("商品いろはにほへと") + ")");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:insert  order (ORDER_NAME=" + dq("order2") + ", CUSTOMER_NAME=" + dq("お客様B") + ", PRODUCT_NAME=" + dq("商品えひもせすん") + ")");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:insert  order (ORDER_NAME=" + dq("order3") + ", CUSTOMER_NAME=" + dq("お客様B") + ", PRODUCT_NAME=" + dq("商品XYZ") + ")");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:commit");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+
+        r = driver.sendQuery("please:transaction");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:update order  (PRODUCT_NAME=" + dq("テスト商品００９") + ")  ");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:commit");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+
+        r = driver.sendQuery("please:transaction");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please: select order   ");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        ASSERT_EQ(3, r.rows.size());
+        std::sort(r.rows.begin(), r.rows.end(),
+                  [](std::map<std::string, std::string> a, std::map<std::string, std::string> b) {
+                      return a.at("order_name") < b.at("order_name");
+                  });
+
+        for (int i = 0; i < r.rows.size(); ++i) {
+            const auto row = r.rows[i];
+            switch (i) {
+            case 0:
+                ASSERT_STREQ("order1", row.at("order_name").c_str());
+                ASSERT_STREQ("お客様A", row.at("customer_name").c_str());
+                ASSERT_STREQ("テスト商品００９", row.at("product_name").c_str());
+                break;
+            case 1:
+                ASSERT_STREQ("order2", row.at("order_name").c_str());
+                ASSERT_STREQ("お客様B", row.at("customer_name").c_str());
+                ASSERT_STREQ("テスト商品００９", row.at("product_name").c_str());
+                break;
+            case 2:
+                ASSERT_STREQ("order3", row.at("order_name").c_str());
+                ASSERT_STREQ("お客様B", row.at("customer_name").c_str());
+                ASSERT_STREQ("テスト商品００９", row.at("product_name").c_str());
+                break;
+            default:
+                ASSERT_FALSE(true);
+            }
+        }
+    }
+
+    TEST_F(DatabaseTest, delete_001)
+    {
+        Database db{};
+        db.start();
+        PapierMache::DbStuff::Connection con = db.getConnection();
+        Driver driver{con};
+        Driver::Result r = driver.sendQuery("please:user admin adminpass");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:transaction   ");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:insert  order (ORDER_NAME=" + dq("order1") + ", CUSTOMER_NAME=" + dq("お客様A") + ", PRODUCT_NAME=" + dq("商品いろはにほへと") + ")");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:insert  order (ORDER_NAME=" + dq("order2") + ", CUSTOMER_NAME=" + dq("お客様B") + ", PRODUCT_NAME=" + dq("商品えひもせすん") + ")");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:insert  order (ORDER_NAME=" + dq("order3") + ", CUSTOMER_NAME=" + dq("お客様B") + ", PRODUCT_NAME=" + dq("商品XYZ") + ")");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:commit");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+
+        r = driver.sendQuery("please:transaction");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:delete order  (CUSTOMER_NAME=" + dq("お客様B") + ") ");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:commit");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+
+        r = driver.sendQuery("please:transaction");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please: select order   ");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        ASSERT_EQ(1, r.rows.size());
+
+        for (int i = 0; i < r.rows.size(); ++i) {
+            const auto row = r.rows[i];
+            switch (i) {
+            case 0:
+                ASSERT_STREQ("order1", row.at("order_name").c_str());
+                ASSERT_STREQ("お客様A", row.at("customer_name").c_str());
+                ASSERT_STREQ("商品いろはにほへと", row.at("product_name").c_str());
+                break;
+            default:
+                ASSERT_FALSE(true);
+            }
+        }
+    }
+
+    TEST_F(DatabaseTest, delete_002)
+    {
+        Database db{};
+        db.start();
+        PapierMache::DbStuff::Connection con = db.getConnection();
+        Driver driver{con};
+        Driver::Result r = driver.sendQuery("please:user admin adminpass");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:transaction   ");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:insert  order (ORDER_NAME=" + dq("order1") + ", CUSTOMER_NAME=" + dq("お客様A") + ", PRODUCT_NAME=" + dq("商品いろはにほへと") + ")");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:insert  order (ORDER_NAME=" + dq("order2") + ", CUSTOMER_NAME=" + dq("お客様B") + ", PRODUCT_NAME=" + dq("商品えひもせすん") + ")");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:insert  order (ORDER_NAME=" + dq("order3") + ", CUSTOMER_NAME=" + dq("お客様B") + ", PRODUCT_NAME=" + dq("商品XYZ") + ")");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:commit");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+
+        r = driver.sendQuery("please:transaction");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:delete order   ");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please:commit");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+
+        r = driver.sendQuery("please:transaction");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        r = driver.sendQuery("please: select order   ");
+        LOG << r.isSucceed << ": " << r.message;
+        if (!r.isSucceed) ASSERT_FALSE(true);
+        ASSERT_EQ(0, r.rows.size());
     }
 
 } // namespace PapierMache::DbStuff
